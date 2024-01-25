365765cac317e725c81ab630adef05dff2692883
doonay <dementiado@gmail.com>
2024-01-25 12:53:49 +0300

    Add files via upload

--- parser/get_logs.py
+++ parser/get_logs.py
@@ -3,11 +3,11 @@ from loguru import logger
 
 # debug thing
 current_module = os.path.basename(__file__)
-# logging
-def get_logs(message):
+
+def get_logs(current_module, message):
 	message = f'{current_module}: {message}'
 	logger.add('debug.log', format='{time:DD:MM:YY hh:mm} | {level} | {message}', level='DEBUG', rotation='1MB', compression='zip')
 	logger.debug(f'{message}')
 
 if __name__ == '__main__':
-	logging('Hello, loguru!')
+	get_logs(current_module, 'Hello, loguru!')
--- parser/get_pagination.py
+++ parser/get_pagination.py
@@ -2,9 +2,13 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from loguru import logger
+from get_logs import get_logs
+
+# debug thing
+current_module = os.path.basename(__file__)
 
 @logger.catch
-def main(url :str) -> int: 
+def get_pagination(url :str) -> int: 
     headers = {
     	'cookie': 'vamhubconsent=yes',
     }
@@ -12,14 +16,10 @@ def main(url :str) -> int:
     soup = BeautifulSoup(r.text, 'lxml')
     navs = soup.find('ul', {'class': 'pageNav-main'}).find_all('li')
     pages = int(navs[-1].get_text())
+    get_logs(current_module, pages)
     return pages
 
 if __name__ == '__main__':
 	url = 'https://hub.virtamate.com/resources/categories/looks.7/'
-	pages: int = main(url)
-	print(pages)
-	#Логгирование
-	current_module = os.path.basename(__file__)
-	message = f'Модуль {current_module}: {pages}'
-	logger.add('debug.log', format='{time:DD:MM:YY hh:mm} | {level} | {message}', level='DEBUG', rotation='1MB', compression='zip')
-	logger.debug(f'{message}')
+	pages: int = get_pagination(url)
+	
